1be780afd8ba4ce3fe708c59ac1547c7ae420306
suyogschavan <suyogschavan03@gmail.com>
2023-04-29 00:35:12 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# PBL
-PBL project ML
+# PBL_execution
+
+## Steps to run
+
+1. Download the object_detection.h5 file from [GDrive](https://drive.google.com/file/d/1wHyv3muz3HbHY88QHReUF8V6YFPyABEd/view?usp=share_link "Suyog's Drive")
+2. Copy it and past in Model folder.
+3. Install all the packages given below in Requirements.
+4. Run app.py
+5. Done.
+
+# Requirements:
+
+* numpy
+* opencv-python
+* matplotlib
+* tensorflow
+* pytesseract
+* tensorflow
+* flask
+* tesseract
+* tesseract-ocr
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import os
+from deeplearning import OCR
+# webserver gateway interface
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+app = Flask(__name__)
+
+BASE_PATH = os.getcwd()
+UPLOAD_PATH = os.path.join(
+    BASE_PATH, './static/upload')
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        upload_file = request.files['image_name']
+        filename = upload_file.filename
+        path_save = os.path.join(UPLOAD_PATH, filename)
+        upload_file.save(path_save)
+        text = OCR(path_save, filename)
+
+        return render_template('index.html', upload=True, upload_image=filename, text=text)
+
+    return render_template('index.html', upload=False)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
